cc9f5f8c946e548d511b558de107167dde076c70
Jose Norberto SANCHEZ <josensanchez@gmail.com>
2023-04-12 10:56:20 -0300

    php and typescript

--- mappings.lua
+++ mappings.lua
@@ -8,6 +8,7 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
+    ["<leader>dd"] = { "<cmd> lua vim.diagnostic.open_float() <CR>", desc = "?   toggles local troubleshoot" },
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
